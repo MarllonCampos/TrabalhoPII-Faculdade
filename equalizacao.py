30d87a58e8883d9f01d16d3233ee320c14391a89
MarllonCampos <marlloncampos@outlook.com>
2020-09-28 22:03:31 -0300

    Criado funcionalidade para pegar o menor e o maior valor do array

--- equalizacao.py
+++ equalizacao.py
@@ -4,3 +4,6 @@ qtdPixels = int(input('Quantos pixels você ira inserir? '))
 for i in range(qtdPixels):
     vetorComNumeros.append(int(input('Qual o valor do pixel? ')))
     
+menorValor = min(vetorComNumeros)
+maiorValor = max(vetorComNumeros)
+
